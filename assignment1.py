459ce308715093eaed7ae3ac552e47ed06a4673d
ayush1208 <ayush.mishra1208@gmail.com>
2020-08-20 23:24:39 +0530

    Changes done

--- assignment1.py
+++ assignment1.py
@@ -4,13 +4,19 @@ def addition(a,b):
 def subtraction(a,b):
 	return a-b;
 
+def mul(a,b):
+	return a*b
+
+def div(a,b):
+	return a/b
 
 if __name__ == "__main__":
-	a = 3
+	a = 15
 	b = 5
 	print(addition(a,b))
 	print(subtraction(a,b))
-
+	print(mul(a,b))
+	print(div(a,b))
 
 '''
 clone from github
